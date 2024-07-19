4241fa9743901a1167bd0cbf49896fe8ec9aac9a
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-07-19 22:46:25 +0300

    rearrange: added class FinishedMatchesPersistenceService  which added ended matches in DB

---
+++ src/main/java/com/example/hibernate_practice/service/FinishedMatchesPersistenceService.java
@@ -0,0 +1,17 @@
+package com.example.hibernate_practice.service;
+
+import com.example.hibernate_practice.dao.MatchDAO;
+import com.example.hibernate_practice.model.Match;
+
+
+public class FinishedMatchesPersistenceService {
+    MatchDAO matchDAO;
+
+    public FinishedMatchesPersistenceService() {
+        matchDAO = MatchDAO.getInstance();
+    }
+
+    public void addMatch(Match match) {
+        matchDAO.saveMatch(match);
+    }
+}
