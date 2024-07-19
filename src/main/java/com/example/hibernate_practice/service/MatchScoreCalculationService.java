7c1b4f9b57858962c38bf4d231294dc61f3f40c8
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-07-19 22:54:34 +0300

    fix: fixed the logic of scoring on sets

--- src/main/java/com/example/hibernate_practice/service/MatchScoreCalculationService.java
+++ src/main/java/com/example/hibernate_practice/service/MatchScoreCalculationService.java
@@ -1,86 +1,93 @@
 package com.example.hibernate_practice.service;
 
-
 import com.example.hibernate_practice.model.EPlayer;
 import com.example.hibernate_practice.model.MatchScore;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 public class MatchScoreCalculationService {
-    MatchScore matchScore;
-
-    public MatchScore getMatchScore() {
-        return matchScore;
-    }
+    @Setter
+    private MatchScore matchScore;
 
     public MatchScoreCalculationService() {
         this.matchScore = new MatchScore();
-        matchScore.clearTieBreakPoints();
-        matchScore.clearPoints();
-        matchScore.clearSets();
     }
 
     public void playerWins15Points(EPlayer player) {
-        if (matchScore.getPlayerPoints(player) == 30)
-            matchScore.setPlayerPoints(player, 40);
+        int currentPoints = this.matchScore.getPlayerPoints(player);
 
-         else if (matchScore.getPlayerPoints(player) == 40) {
-            matchScore.setPlayerGames(player, 1);
-            matchScore.setPlayerPoints(player, 0);
-
-            if (player.ordinal() == 0)
-                matchScore.setPlayerPoints(EPlayer.SECOND_PLAYER, 0);
-             else if (player.ordinal() == 1)
-                matchScore.setPlayerPoints(EPlayer.FIRST_PLAYER, 0);
-
-        } else
-            matchScore.setPlayerPoints(player, matchScore.getPlayerPoints(player) + 15);
-
+        if (currentPoints == 30) {
+            this.matchScore.setPlayerPoints(player, 40);
+        } else if (currentPoints == 40) {
+            playerWinsGame(player);
+        } else {
+            this.matchScore.setPlayerPoints(player, currentPoints + 15);
+        }
     }
 
     public void playerWinsGame(EPlayer player) {
-        if (matchScore.getPlayerGames(EPlayer.FIRST_PLAYER) == 6
-                || matchScore.getPlayerGames(EPlayer.SECOND_PLAYER) == 6) {
-            startTieBreak();
+        int opponentGames = 0;
+        int playerGames = this.matchScore.getPlayerGames(player);
+        if (player == EPlayer.FIRST_PLAYER) {
+            opponentGames = this.matchScore.getPlayerGames(EPlayer.SECOND_PLAYER);
+        } else if (player == EPlayer.SECOND_PLAYER) {
+            opponentGames = this.matchScore.getPlayerGames(EPlayer.FIRST_PLAYER);
         }
 
-        if (matchScore.getPlayerGames(player) == 6) {
-            matchScore.setPlayerGames(player, 0);
+        if (playerGames == 5 && opponentGames == 6 || playerGames == 6 && opponentGames == 5) {
+            this.matchScore.setPlayerGames(EPlayer.FIRST_PLAYER, 6);
+            this.matchScore.setPlayerGames(EPlayer.SECOND_PLAYER, 6);
+            startTieBreak();
+        } else if (playerGames == 6) {
             playerWinsSet(player);
-            if (player.ordinal() == 0)
-                matchScore.setPlayerPoints(EPlayer.SECOND_PLAYER, 0);
-            else if (player.ordinal() == 1)
-                matchScore.setPlayerPoints(EPlayer.FIRST_PLAYER, 0);
+        } else {
+            this.matchScore.increasePlayerGames(player);
+        }
 
-        } else
-            matchScore.increasePlayerGames(player);
+
+        this.matchScore.setPlayerPoints(EPlayer.FIRST_PLAYER, 0);
+        this.matchScore.setPlayerPoints(EPlayer.SECOND_PLAYER, 0);
     }
 
     public void playerWinsSet(EPlayer player) {
-        if (matchScore.getPlayerSets(player) == 1) {
-            matchScore.setPlayerSets(player, 2);
-            matchScore.increasePlayerSets(player);
-        } else
-            matchScore.increasePlayerSets(player);
-    }
-
-    public MatchScore playerWinsMatch(EPlayer player) {
-        matchScore.setWinner(player);
-        matchScore.setMatchFinished(true);
-        return matchScore;
+        int playerSets = this.matchScore.getPlayerSets(player);
+        if (playerSets == 1) {
+            this.matchScore.setPlayerSets(player, 2);
+            this.matchScore.setWinner(player);
+            this.matchScore.setMatchFinished(true);
+        } else {
+            this.matchScore.increasePlayerSets(player);
+        }
+        this.matchScore.setPlayerGames(EPlayer.FIRST_PLAYER, 0);
+        this.matchScore.setPlayerGames(EPlayer.SECOND_PLAYER, 0);
+        this.matchScore.setPlayerPoints(EPlayer.FIRST_PLAYER, 0);
+        this.matchScore.setPlayerPoints(EPlayer.SECOND_PLAYER, 0);
     }
 
     public void playerWinsTieBreakPoint(EPlayer player) {
+        this.matchScore.increaseTieBreakPoints(player);
 
+        int pointsPlayer1 = this.matchScore.getPlayerTieBreakPoints(EPlayer.FIRST_PLAYER);
+        int pointsPlayer2 = this.matchScore.getPlayerTieBreakPoints(EPlayer.SECOND_PLAYER);
 
+        if (pointsPlayer1 >= 7 && pointsPlayer1 > pointsPlayer2 + 1) {
+            playerWinsSet(EPlayer.FIRST_PLAYER);
+            this.matchScore.tieBreakEnded();
+            this.matchScore.increaseTieBreakPoints(EPlayer.SECOND_PLAYER);
+            this.matchScore.clearTieBreakPoints();
+        } else if (pointsPlayer2 >= 7 && pointsPlayer2 > pointsPlayer1 + 1) {
+            playerWinsSet(EPlayer.SECOND_PLAYER);
+            this.matchScore.tieBreakEnded();
+            this.matchScore.clearTieBreakPoints();
+        }
     }
-
 
     public void startTieBreak() {
-        matchScore.setTieBreak(true);
-
-
+        this.matchScore.setTieBreak(true);
     }
 
-
-    // до 40 поинтов, до 6 (7) геймов, до двух сетов
-    // если выиграл один гейм, то все поинты у противника обнуляются
+    public boolean isPlayerWin(EPlayer player) {
+        return this.matchScore.getWinner() == player;
+    }
 }
