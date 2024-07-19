e68003a89791de43a0e624d64af91e6fd2b4b9ae
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-07-19 22:43:18 +0300

    feat: added forward on .jsp page from GET method to POST method which do forward on GET method in MatchScoreController

--- src/main/java/com/example/hibernate_practice/controllers/CreateMatchController.java
+++ src/main/java/com/example/hibernate_practice/controllers/CreateMatchController.java
@@ -1,5 +1,8 @@
 package com.example.hibernate_practice.controllers;
 
+import com.example.hibernate_practice.model.Match;
+import com.example.hibernate_practice.service.NewMatchService;
+import com.example.hibernate_practice.service.OngoingMatchesService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,15 +11,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "create_Match_Controller", value = "/new-match")
+@WebServlet(name = "create_match_controller", value = "/new-match/*")
 public class CreateMatchController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("CreateMatchPage.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/CreateMatchPage.jsp").forward(req, resp);
     }
 
+    private static String uuid;
+
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
+        String player1 = req.getParameter("player1");
+        String player2 = req.getParameter("player2");
+        NewMatchService newMatchService = new NewMatchService();
+        Match match = newMatchService.createMatch(player1, player2);
+        uuid = OngoingMatchesService.addMatch(match);
+        resp.sendRedirect("match-score/?uuid=" + uuid);
     }
 }
