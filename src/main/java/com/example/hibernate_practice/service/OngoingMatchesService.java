72ef566473992b09f4b14510d90daf2be481c9ad
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-07-19 22:59:31 +0300

    feat: added methods:static String addMatch(Match match), static void addMatchNotUniqueUUID(Match match, String uuid), static String generateUniqueUUID(), static Optional<Match> getMatch(String uuid)

--- src/main/java/com/example/hibernate_practice/service/OngoingMatchesService.java
+++ src/main/java/com/example/hibernate_practice/service/OngoingMatchesService.java
@@ -1,14 +1,38 @@
 package com.example.hibernate_practice.service;
 
 import com.example.hibernate_practice.model.Match;
-import com.example.hibernate_practice.model.Player;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class OngoingMatchesService {
+    private static Map<String, Match> onGoingMatches = new ConcurrentHashMap<>();
 
-    public Match getMatch(Player player1, Player player2) {
-        Match match = new Match(player1, player2, null);
-        return match;
+    public static String addMatch(Match match) {
+        String uuid = generateUniqueUUID();
+        onGoingMatches.put(uuid, match);
+        return uuid;
     }
 
+    public static void addMatchNotUniqueUUID(Match match, String uuid) {
+        onGoingMatches.put(uuid, match);
+    }
 
+    private static String generateUniqueUUID() {
+        String uuid;
+        do {
+            uuid = UUID.randomUUID().toString();
+        } while (onGoingMatches.containsKey(uuid));
+        return uuid;
+    }
+
+    public static Optional<Match> getMatch(String uuid) {
+        return Optional.ofNullable(onGoingMatches.get(uuid));
+    }
+
+    public static void removeMatch(String uuid) {
+        onGoingMatches.remove(uuid);
+    }
 }
