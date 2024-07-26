7f1872490137217942daf6aeb910cf6284f8bf01
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-07-26 03:18:42 +0300

    style: methods renamed clarifications have been removed

--- src/main/java/com/example/hibernate_practice/service/NewMatchService.java
+++ src/main/java/com/example/hibernate_practice/service/NewMatchService.java
@@ -9,22 +9,22 @@ import java.util.Optional;
 public class NewMatchService {
     public Match createMatch(String player1Name, String player2Name) {
         PlayerDAO playerDAO = PlayerDAO.getInstance();
-        Optional<Player> optPlayer1 = playerDAO.findPlayerByName(player1Name);
-        Optional<Player> optPlayer2 = playerDAO.findPlayerByName(player2Name);
+        Optional<Player> optPlayer1 = playerDAO.findByName(player1Name);
+        Optional<Player> optPlayer2 = playerDAO.findByName(player2Name);
 
         Player player1;
         Player player2;
 
         if (optPlayer1.isEmpty()) {
             player1 = new Player(player1Name);
-            playerDAO.savePlayer(player1);
+            playerDAO.save(player1);
         } else {
             player1 = optPlayer1.get();
         }
 
         if (optPlayer2.isEmpty()) {
             player2 = new Player(player2Name);
-            playerDAO.savePlayer(player2);
+            playerDAO.save(player2);
         } else {
             player2 = optPlayer2.get();
         }
